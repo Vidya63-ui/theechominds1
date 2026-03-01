c220f3ce00c0f30a7cf7f792d81d967512e76a5a
Vidya63-ui <tupevidyasagar@gmail.com>
2026-03-01 21:53:53 +0530

    cors change

--- TheEchoMinds/server/index.js
+++ TheEchoMinds/server/index.js
@@ -18,17 +18,20 @@ const allowedOrigins = (process.env.CLIENT_URL
   ? process.env.CLIENT_URL.split(",").map((v) => v.trim())
   : ["http://localhost:5173", "http://127.0.0.1:5173"]).filter(Boolean);
 
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Origin not allowed by CORS"));
-    },
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 
